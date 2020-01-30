1feb53f634100cd849c5062878f3032611ece69f
matthewlyf <51236438+matthewlyf@users.noreply.github.com>
2020-01-30 18:58:13 -0500

    Update comment stream_git.py

--- comment stream_git.py
+++ comment stream_git.py
@@ -1,14 +1,12 @@
 '''
-scrape_reddit(subreddit_name, reddit, client)
-word_freq_filt(all_col_toke)
+scrpr(subreddit_name, reddit, client)
+wrdfrq(all_col_toke)
 '''
-class subreddit_snp:
-  def __init__(self, y0, y1, y2):
-     self.y0 = y0
-     self.y1 = y1
-     self.y2 = y2
+
+
+
      
-def scrape_reddit(subreddit_name, reddit, client):
+def scrpr(subreddit_name, reddit, client):
     '''takes 3 variables
     subreddit name should be the exact string or variable containing string of
     subreddit name
@@ -103,7 +101,8 @@ def scrape_reddit(subreddit_name, reddit, client):
     for submission in top_subreddit:
         all_col = mydb['dictionary']
         all_col_raw = mydb['dictionary_raw']
-        scrape_reddit.all_col_toke = mydb['dictionary_token']
+        
+        scrpr.all_col_toke = mydb['dictionary_token']
         topics_dict = { "title": submission.title , "score":submission.score, 
                        "id":submission.id, "url":submission.url, 
                        "comms_num":submission.num_comments , 
@@ -156,12 +155,12 @@ def scrape_reddit(subreddit_name, reddit, client):
                 else:
                     lemma = wnl.lemmatize(word, wntag)
                     post_lemm.append(lemma)
-                    '''this conditional statement can be removed at a 
-                    later point in time'''
-                    if word != lemma:
-                        print("i've been shortened from ", word, lemma)
-                    else:
-                        pass
+#                    '''this conditional statement can be removed at a 
+#                    later point in time'''
+#                    if word != lemma:
+#                        print("i've been shortened from ", word, lemma)
+#                    else:
+#                        passS
 #Step 9: Remove Stop words and re-join comments     
             filtered_sentence = [] 
             for w in post_lemm: 
@@ -176,7 +175,7 @@ def scrape_reddit(subreddit_name, reddit, client):
             filtered_sentence = filtered_sentence.split()
             filtered_sentence = [i for i in filtered_sentence if len(i) > 1]
             token_filtered_sent = {"message":filtered_sentence}
-            scrape_reddit.all_col_toke.insert_one(token_filtered_sent)
+            scrpr.all_col_toke.insert_one(token_filtered_sent)
             filtered_sentence = ' '.join(filtered_sentence)
 #Step 10: Store processed messages in mongoDB        
             mydict2 = {"message":filtered_sentence}
@@ -184,36 +183,64 @@ def scrape_reddit(subreddit_name, reddit, client):
         print("Done", submission.title)
             #comment_body =  comment_body + comment.body + "\n"
     print("Done scraping")
-    y0 = mydb['dictionary_token']
-    y1 = mydb['dictionary']
-    y2 = mydb['dictionary_raw']
-    return subreddit_snp(y0,y1,y2)
+    
     
 
-def word_freq_filt(all_col_toke):
+def wrdfrq(all_col_toke):
     '''takes dataframe with tokenized messages as variable input,
     if scrape_reddit() was run, it can take variable all_col_toke'''
-#Step 0: Prepare libraries, database, and dataframes
+    #Step 0: Prepare libraries, database, and dataframes
     import pandas as pd
     from collections import defaultdict
     frequency = defaultdict(int)
     new_frequency = defaultdict(int)
     data_token = pd.DataFrame(list(all_col_toke.find()))
     processed_messages = []
-#Step 1: Count words and record frequency
+    #Step 1: Count words and record frequency
     for messages in data_token['message']:
         for text in messages:
             frequency[text] += 1 
-#Step 2: Assuming words appearing once are unimportant, removed words appearing
-#less than 2 times
+    #Step 2: Assuming words appearing once are unimportant, removed words appearing
+    #less than 2 times
         processed_corpus = [text for text in messages if frequency[text] >2]
         for text in processed_corpus:
             new_frequency[text] += 1
-#Step 3: Take filtered messages and create final list
+    #Step 3: Take filtered messages and create final list
         processed_messages.append(processed_corpus)
-#Step 4: Convert final frequncy data into a dataframe   
+    #Step 4: Convert final frequncy data into a dataframe   
     wordfrequency = pd.DataFrame([new_frequency])
     wordfrequency= wordfrequency.T
     wordfrequency= wordfrequency.reset_index()
-    word_freq_filt.wordfrequency = wordfrequency.rename(columns= {"index" : "word", 0 : "Count"})
-
+    wrdfrq.wordfrequency = wordfrequency.rename(columns= {"index" : "word", 0 : "Count"})
+    wrdfrq.processed_messages = processed_messages
+    return processed_messages, len(new_frequency)
+def tfidf(query, tokenized_list):
+    #Step 1: Create corpus from processed messages(this can be accessed from 
+    #wrdfrq.processed_messages)    
+    import gensim as gm
+    import pandas as pd
+    dictionary = gm.corpora.Dictionary(tokenized_list)
+    bow_corpus = [dictionary.doc2bow(text) for text in tokenized_list]
+    #Step 2: Create TF-IDF model from corpus
+    tfidf = gm.models.TfidfModel(bow_corpus)
+    #TODO num_features is the value of unique tokens from dictionary, need
+    #to extract that value
+    index = gm.similarities.SparseMatrixSimilarity(tfidf[bow_corpus], num_features=7014) 
+    query_document = query.split()
+    query_bow = dictionary.doc2bow(query_document)
+    sims = index[tfidf[query_bow]]
+    doc_no = []
+    score_lst = []
+    msg = []   
+    #returns top documents containing query and puts it in a dataframe.  This dataframe can be used for sentiment analysis
+    for document_number, score in sorted(enumerate(sims), key=lambda x: x[1], reverse=True):
+        #print(document_number, score, data['message'][document_number])
+        if score > 0:
+            doc_no.append(document_number)
+            score_lst.append(score)
+            msg.append(data['message'][document_number])#this is the result used in sentiment analysis
+        else:
+            pass
+    result = pd.concat([pd.Series(doc_no, name = "doc_no"), pd.Series(score_lst, name = "score"), pd.Series(msg, name = 'msg')], axis=1)
+    print(result.head())    
+#
